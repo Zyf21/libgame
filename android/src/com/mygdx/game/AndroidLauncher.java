a608ab0b97dcdec92bf4182f436552778a0fb26c
dmtech <ivanovwfgkz@yandex.by>
2020-08-07 13:18:41 +0300

    штше

---
+++ android/src/com/mygdx/game/AndroidLauncher.java
@@ -0,0 +1,20 @@
+package com.mygdx.game;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mygdx.game.mygame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		config.useAccelerometer = false;
+		config.useGyroscope = false;
+		config.useCompass = false;
+
+		initialize(new mygame(), config);
+	}
+}
